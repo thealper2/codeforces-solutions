11eed8600dea065e896984cccbf82ac8767a8b0e
Alper Karaca <thisiseternity12@gmail.com>
2025-09-28 16:33:40 +0300

    1915A

---
+++ 1900-1999/1915A-Odd-One-Out.py
@@ -0,0 +1,9 @@
+n = int(input())
+for _ in range(n):
+	a, b, c = map(int, input().split())
+	if a != b and a != c:
+		print(a)
+	elif b != a and b != c:
+		print(b)
+	elif c != a and c != b:
+		print(c)
