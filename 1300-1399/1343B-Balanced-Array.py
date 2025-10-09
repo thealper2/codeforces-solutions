57c1a4a433108b939ab5782bf128545c57016e04
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-10-09 08:12:15 +0300

    1343B

--- 1300-1399/1343B-Balanced-Array.py
+++ 1300-1399/1343B-Balanced-Array.py
@@ -1,14 +1,12 @@
 t = int(input())
 for _ in range(t):
 	n = int(input())
-	half = n // 2
-	if half % 2 == 1:
+	k = n // 2
+	if k % 2 == 1:
 		print('NO')
 	else:
 		print('YES')
-		evens = [2 * i for i in range(1, half + 1)]
-		odds = [2 * i - 1 for i in range(1, half)]
-		last_odd = sum(evens) - sum(odds)
-		odds.append(last_odd)
-		result = evens + odds
-		print(' '.join(map(str, result)))
+		first_half = [2 * i for i in range(1, k + 1)]
+		second_half = [2 * i - 1 for i in range(1, k)]
+		second_half.append(3 * k - 1)
+		print(' '.join(map(str, first_half + second_half)))
